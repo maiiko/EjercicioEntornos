e57ba634a8a37de6053ad8ed85d21c00b29f6045
Alumno <Alumno@AULA4-7>
2019-04-04 23:42:21 +0200

    actualización 4-4-2019

--- edes/src/edes/Videojuego.java
+++ edes/src/edes/Videojuego.java
@@ -1,5 +1,68 @@
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Videojuego {
 // patata
+	BufferedReader leer=new BufferedReader(new InputStreamReader(System.in));
+	
+	String titulo, genero, compañia;
+	int horasEstimadas;
+	boolean entregado;
+	
+	Videojuego(){
+		titulo="";
+		genero="";
+		compañia="";
+		horasEstimadas=10;
+		entregado=false;
+	}
+	Videojuego(String tit){
+		titulo=tit;
+		genero="";
+		compañia="";
+		horasEstimadas=10;
+		entregado=false;
+	}
+	
+	Videojuego(String tit,String comp,String gen,int h){
+	titulo=tit;
+	genero=gen;
+	compañia=comp;
+	horasEstimadas=h;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getCompañia() {
+		return compañia;
+	}
+
+	public void setCompañia(String compañia) {
+		this.compañia = compañia;
+	}
+
+	public int getHorasEstimadas() {
+		return horasEstimadas;
+	}
+
+	public void setHorasEstimadas(int horasEstimadas) {
+		this.horasEstimadas = horasEstimadas;
+	}
+	
+	
+	
 }
