a2d906aee6d6bb525febad7fc484b21480803242
TheHolyDenier <bloodthirsty.snow.white@gmail.com>
2019-04-04 21:24:05 +0200

    Clase Serie sin el toString

--- edes/src/Serie.java
+++ edes/src/Serie.java
@@ -1,4 +1,64 @@
 
 public class Serie {
+	private String titulo;
+	private int numTemporadas;
+	private boolean entregado;
+	private String genero;
+	private String creador;
+	
+	public Serie() {
+		titulo = "";
+		numTemporadas = 3;
+		entregado = false; 
+		genero = "";
+		creador = "";
+	}
 
+	public Serie(String titulo, String creador) {
+		this.titulo = titulo;
+		this.creador = creador;
+		numTemporadas = 3;
+		entregado = false; 
+		genero = "";
+	}
+	
+	public Serie(String titulo, int numTemporadas, String genero, String creador) {
+		this.titulo = titulo;
+		this.numTemporadas = numTemporadas;
+		this.genero = genero;
+		this.creador = creador;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public int getNumTemporadas() {
+		return numTemporadas;
+	}
+
+	public void setNumTemporadas(int numTemporadas) {
+		this.numTemporadas = numTemporadas;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getCreador() {
+		return creador;
+	}
+
+	public void setCreador(String creador) {
+		this.creador = creador;
+	}
+	
 }
